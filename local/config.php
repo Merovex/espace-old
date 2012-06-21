48cf1708afc4a8ed23f64e3f7f844f5137b199b1
Ben Wilson <dausha@gmail.com>
2012-06-20 21:34:08 -0400

    Mediatable.

--- local/config.php
+++ local/config.php
@@ -43,8 +43,8 @@ include_once("$FarmC/social/blogsimple.php");
 #==================================
 $FmtPV['$GroupTitle'] = '(($t = PageVar("$group.GroupAttributes", \'$Title\')) == "GroupAttributes") ? $AsSpacedFunction($group) : $t';
 
-include_once("$FarmC/mediacat.php");
-include_once("$FarmC/mediatable.php");
+include_once("$FarmC/markup/mediacat.php");
+include_once("$FarmC/markup/mediatable.php");
 include_once("$FarmC/translate_this.php");
 include_once("$FarmC/markup/grouptitle.php");
 include_once("$FarmC/markup/newpageboxplus.php");
