82ad4e5f910e8e0ed6e374228e1aa44025749e22
Ben Wilson <dausha@gmail.com>
2012-06-20 21:57:35 -0400

    Mediatable.

--- local/config.php
+++ local/config.php
@@ -88,7 +88,7 @@ include_once("social.php");
 if ($Author) {
   $UserText =<<<EOT
     <div class="btn-group pull-right">
-      <a class="btn btn-primary" href="#"><i class="icon-user icon-white"></i> $Author</a>
+      <a class="btn btn-primary" href="/Contributers/$Author"><i class="icon-user icon-white"></i> $Author</a>
       <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
       <ul class="dropdown-menu">
         <li><a href="?action=edit"><i class="icon-pencil"></i> Edit</a></li>
